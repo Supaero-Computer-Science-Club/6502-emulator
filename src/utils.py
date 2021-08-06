9035719e5ff0636dab027075151f36d34f05a0ad
Antoine Stevan <stevan.antoine@gmail.com>
2021-08-06 13:23:10 +0200

    Tools + useless files removed.

--- src/utils.py
+++ src/utils.py
@@ -22,3 +22,16 @@ def to_hex(number, nb_chars=2):
     return hex(number)[2:][::-1].ljust(nb_chars, '0')[::-1]
 
 
+def print_pins(pins, args, end='\n'):
+    bin_pins = to_bin(pins, 40)
+    ctrl1, data, addr, ctrl2 = bin_pins[:9], bin_pins[9:17], bin_pins[17:33], bin_pins[33:]
+    msg = f"{ctrl1} "\
+          f"{data}-({to_hex(int('0b' + data, 2), 2)}) "\
+          f"{addr}-({to_hex(int('0b' + addr, 2), 4)}) "\
+          f"{ctrl2}"
+    if len(args) > 0:
+        msg += ' '
+    for arg, l, s in args:
+        msg += f" {to_bin(arg, l)}"
+        if s: msg += f"-({s})"
+    print('\r' + msg, end=end)
